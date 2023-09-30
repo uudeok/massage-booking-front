957c443a2d6e7514dead25bca58db1f571933cdb
YuDeok <awesome36@daum.net>
2023-09-30 16:41:26 +0900

    styled-components reset css 적용

---
+++ src/styles/globalStyles.tsx
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+    ${reset}
+    
+    * {
+        box-sizing: border-box;
+    }
+
+    ol, ul {
+        list-style: none;
+    }
+
+`;
+
+export default GlobalStyles;
