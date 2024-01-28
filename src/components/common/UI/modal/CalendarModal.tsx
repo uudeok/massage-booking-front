40ccf67ca20749010f8a83ab60500f666348e6c6
YuDeok <awesome36@daum.net>
2024-01-28 13:14:40 +0900

    test

--- src/components/common/UI/modal/CalendarModal.tsx
+++ src/components/common/UI/modal/CalendarModal.tsx
@@ -1,5 +1,4 @@
 import { Time } from "../../dropdown";
-import React from "react";
 import styled from "styled-components";
 import Calendar from "../../../calendar";
 import CommonButton from "../../button/CommonButton";
