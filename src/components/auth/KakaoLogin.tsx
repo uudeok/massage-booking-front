bc21bd8f221968736d1e697c1b96302daa9f1bce
YuDeok <awesome36@daum.net>
2024-02-15 00:32:52 +0900

    update : react-cookie 라이브러리 설치

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -4,9 +4,7 @@ import CommonButton from "../common/button/CommonButton";
 // 여기는 카카오 로그인 버튼 컴포넌트
 
 const KakaoLogin = () => {
-  const REST_API_KEY = process.env.REACT_APP_KAKAO_REST_KEY;
-  const REDIRECT_URL = "http://localhost:3000/kakaoCallback";
-  const KAKAO_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code`;
+  const KAKAO_URL = `http://localhost:5000/auth/kakao`;
 
   return (
     <Link to={KAKAO_URL}>
