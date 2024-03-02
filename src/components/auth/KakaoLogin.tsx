4e3ea1afd6408122a86521c69788beab1849c796
YuDeok <awesome36@daum.net>
2024-03-02 23:21:17 +0900

    build test ( 환경 별 변수 참조 버그 확인 )

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -1,7 +1,7 @@
 import CommonButton from "../common/button/CommonButton";
 
 const Rest_api_key = `${process.env.REACT_APP_KAKAO_REST_KEY}`;
-const redirect_uri = `${process.env.REACT_APP_API_URL}/auth/kakao/callback`;
+const redirect_uri = `https://www.naturalhealingspa.co.kr/auth/kakao/callback`;
 
 const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
