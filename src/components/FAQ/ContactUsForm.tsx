5e5016aec473170ec9361df0e2d40a97852ec0e8
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-03-21 18:00:02 +0900

    react-hook-form 설치 및 prettier 재설정

--- src/components/FAQ/ContactUsForm.tsx
+++ src/components/FAQ/ContactUsForm.tsx
@@ -1,57 +1,59 @@
-import React, { FC, useRef } from "react";
-import {useForm} from "react-hook-form"
-import emailjs from "@emailjs/browser";
-
+import React, { FC, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import emailjs from '@emailjs/browser';
 
 type FormValue = {
-  title : string;
-  email :string;
-  content : string;
-}
+    title: string;
+    email: string;
+    content: string;
+};
 
+const ContactUsForm: FC = () => {
+    const form = useRef<HTMLFormElement>(null);
 
-const ContactUsForm : FC = () => {
-  const form = useRef<HTMLFormElement>(null);
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<FormValue>();
 
-const {register, handleSubmit , watch, formState : { errors }} = useForm<FormValue>()
+    const onSubmit = (data: any) => {
+        console.log(data);
+    };
 
-const onSubmit = (data : any)  => {
-  console.log(data)
-}
+    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
 
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+        if (!form.current) {
+            console.log('error');
+            return;
+        } else {
+            emailjs
+                .sendForm('service_0vpblui', 'template_jbwr0qy', form.current, {
+                    publicKey: 'tuwilzPgDWn_9AcUJ',
+                })
+                .then(
+                    () => {
+                        console.log('SUCCESS!');
+                    },
+                    (error) => {
+                        console.log('FAILED...', error.text);
+                    },
+                );
+        }
+    };
 
-    if (!form.current) {
-      console.log("error");
-      return;
-    } else {
-      emailjs
-        .sendForm("service_0vpblui", "template_jbwr0qy", form.current, {
-          publicKey: "tuwilzPgDWn_9AcUJ",
-        })
-        .then(
-          () => {
-            console.log("SUCCESS!");
-          },
-          (error) => {
-            console.log("FAILED...", error.text);
-          }
-        );
-    }
-  };
-
-  return (
-  <form onSubmit={handleSubmit(onSubmit)}>
-    <label>제목</label>
-<input {...register("title")} />
-<label>이메일</label>
-<input {...register("email")} />
-<label>내용</label>
-<input {...register("content")} />
-  </form>
-  
-  )
-}
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <label>제목</label>
+            <input {...register('title')} />
+            <label>이메일</label>
+            <input {...register('email')} />
+            <label>내용</label>
+            <input {...register('content')} />
+        </form>
+    );
+};
 
 export default ContactUsForm;
