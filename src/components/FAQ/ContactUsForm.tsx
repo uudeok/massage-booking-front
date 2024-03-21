28a1d331113b5b971a14e4e3fc5f0983cf54edab
서유덕 <duu@seoyudeog-ui-MacBookPro.local>
2024-03-21 21:13:49 +0900

    test

--- src/components/FAQ/ContactUsForm.tsx
+++ src/components/FAQ/ContactUsForm.tsx
@@ -1,59 +1,83 @@
 import React, { FC, useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import emailjs from '@emailjs/browser';
+import styled from 'styled-components';
 
 type FormValue = {
-    title: string;
-    email: string;
-    content: string;
+	title: string;
+	email: string;
+	content: string;
 };
 
 const ContactUsForm: FC = () => {
-    const form = useRef<HTMLFormElement>(null);
+	const form = useRef<HTMLFormElement>(null);
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<FormValue>();
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<FormValue>();
 
-    const onSubmit = (data: any) => {
-        console.log(data);
-    };
+	const onSubmit = (data: any) => {
+		console.log(data);
+	};
 
-    const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
 
-        if (!form.current) {
-            console.log('error');
-            return;
-        } else {
-            emailjs
-                .sendForm('service_0vpblui', 'template_jbwr0qy', form.current, {
-                    publicKey: 'tuwilzPgDWn_9AcUJ',
-                })
-                .then(
-                    () => {
-                        console.log('SUCCESS!');
-                    },
-                    (error) => {
-                        console.log('FAILED...', error.text);
-                    },
-                );
-        }
-    };
+		if (!form.current) {
+			console.log('error');
+			return;
+		} else {
+			emailjs
+				.sendForm('service_0vpblui', 'template_jbwr0qy', form.current, {
+					publicKey: 'tuwilzPgDWn_9AcUJ',
+				})
+				.then(
+					() => {
+						console.log('SUCCESS!');
+					},
+					(error) => {
+						console.log('FAILED...', error.text);
+					},
+				);
+		}
+	};
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <label>제목</label>
-            <input {...register('title')} />
-            <label>이메일</label>
-            <input {...register('email')} />
-            <label>내용</label>
-            <input {...register('content')} />
-        </form>
-    );
+	return (
+
+		<Self onSubmit={handleSubmit(onSubmit)}>
+            <LabelBoxStyle>
+            <LabelStyle>제목</LabelStyle>
+			<input {...register('title')} />
+            </LabelBoxStyle>
+            <LabelBoxStyle>
+            <LabelStyle>이메일</LabelStyle>
+			<input {...register('email')} />
+            </LabelBoxStyle>
+			<label>내용</label>
+			<input {...register('content')} />
+		</Self>
+	);
 };
 
 export default ContactUsForm;
+
+const Self = styled.form`
+	border: 1px solid black;
+	/* height: 100%; */
+    display: flex;
+    flex-direction: column;
+`;
+
+
+const LabelBoxStyle = styled.div`
+    border : 1px solid black;
+  
+`
+
+const LabelStyle = styled.label`
+
+    border :1px solid red;
+`
--- src/components/common/UI/modal/EmailModal.tsx
+++ src/components/common/UI/modal/EmailModal.tsx
@@ -1,16 +1,26 @@
-import ContactUsForm from "../../../FAQ/ContactUsForm";
-import Modal from "./Modal";
+import theme from '../../../../styles/theme';
+import ContactUsForm from '../../../FAQ/ContactUsForm';
+import Modal from './Modal';
+import styled from 'styled-components';
 
 type EmailModalType = {
-  closeModal: () => void;
+	closeModal: () => void;
 };
 
 const EmailModal = ({ closeModal }: EmailModalType) => {
-  return (
-    <Modal closeModal={closeModal}>
-      <ContactUsForm />
-    </Modal>
-  );
+	return (
+		<Modal closeModal={closeModal} height="30rem">
+			<HeaderStyle>문의 하기</HeaderStyle>
+			<ContactUsForm />
+		</Modal>
+	);
 };
 
 export default EmailModal;
+
+const HeaderStyle = styled.h2`
+	border: 1px solid black;
+	text-align: center;
+	font-size: 30px;
+  margin-bottom: 1rem;
+`;
